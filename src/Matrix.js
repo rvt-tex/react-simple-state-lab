32798b2c0e1e9af085324d274cc451f059ce5657
github username <rvtingling@yahoo.com>
2021-03-08 19:51:05 -0500

    code base updated

--- src/Matrix.js
+++ src/Matrix.js
@@ -17,5 +17,14 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
 }
+
+const pattern2 = []
+for (let i = 0; i < 10; i++) {
+  pattern2[i] = [];
+  for (let j = 0; j < 10; j++) {
+    pattern2[i][j] = "#F00";
+  }
+}
+
+Matrix.defaultProps = { values: pattern2 };
